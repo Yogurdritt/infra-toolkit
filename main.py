39c9c3b2ab77eabd8ed347295db92ad83b8917ee
Gabriel Salazar <salgongablabs19@gmail.com>
2026-07-08 14:28:52 -0600

    tool selection menu

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Infra toolkit")
+#while True:
+   
+print("====================\nInfra Toolkit\n====================\n")
+print("Available tools:\n")
+print("1. DNS Lookup:")
+print("Comming Soon...")
+
+seleccion = int(input("Select a tool from above: "))
+
+match seleccion:
+    case 1:
+        print("DNS Lookup working")
+    case _:
+        print("Not implemented yet")
