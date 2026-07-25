f31a8abb8bf010a57f32fc4f6e703fee0238f855
Gabriel Salazar <salgongablabs19@gmail.com>
2026-07-25 02:11:00 -0600

    Implementation of the traceroute feature; Correction and fixing of other features bugs

--- tools/dns_tools.py
+++ tools/dns_tools.py
@@ -1,20 +1,18 @@
 import dns.resolver
 
+import tools.icmp_tools as icmp_tools
+
 def Run_Dns_Lookup():
 
     domain = input("Enter a domain to resolve: ")   
 
-    if not domain or "." not in domain:
+    if not domain or ("." not in domain and len(domain.split())):
             print("****Invalid domain format.")
             return
-    
-    domain_to_resolve = domain.lower()
 
     allowed_records = ["A","AAAA","CNAME","MX","NS","TXT","SOA"]
 
-    print("available records: \n","  A. AAAA. CNAME. MX. TXT. SOA\n")
-
-    record_selection = input("Select the type of record you want to consult, or if you want to consult all (<type>/all): ")                             
+    record_selection = input("Select the type of record you want to consult (A, AAAA, CNAME, MX, TXT, SOA, all): ")                             
 
     if record_selection.upper() not in allowed_records and record_selection.upper() != "ALL":
         print("****Invalid selecction.")   
@@ -22,41 +20,46 @@ def Run_Dns_Lookup():
           
 
     try: 
-        print(f"\nDomain: {domain_to_resolve}")        
+        print(f"\nDomain: {domain.lower()}")        
 
         if record_selection.upper() == "ALL":
-             for record_type in allowed_records: 
+            
+            for record_type in allowed_records: 
 
                 try:
-                    resolution = dns.resolver.resolve(domain_to_resolve, record_type)
+                
+                    resolution = dns.resolver.resolve(domain.lower(), record_type)
 
                     if record_type == "A": #If statement for formatting after the "Domain: <domain.com>" print
-                         print(f"{record_type} records:\n--------")
-                         for record in resolution:
-                            print(f"- {record}")
+                            print(f"{record_type} records:\n--------")
+                            for record in resolution:
+                                print(f"- {record}")
                     else:
                         print(f"\n{record_type} records:\n--------")
                         for record in resolution:
                             print(f"- {record}")
-                
-                except  dns.resolver.NoAnswer:
-                     print(f"\n***Error, the selected domain does not have any {record_type} record registered.")
-                
-        
-        else:
-                          
-            resolution = dns.resolver.resolve(domain_to_resolve, record_selection.upper())
-
-            print(f"{record_selection.upper()} records:")
-            for record in resolution:
-                    print(f"- {record}")
+                except dns.resolver.NoAnswer:
+                    print(f"\n***Error, the selected domain does not have any {record_type} record registered.")
             
+        else:
+
+            try:                          
+                resolution = dns.resolver.resolve(domain.lower(), record_selection.upper())
+
+                print(f"{record_selection.upper()} records:")
+                for record in resolution:
+                        print(f"- {record}")       
+
+            except  dns.resolver.NoAnswer:
+                    print(f"\n***Error, the selected domain does not have any {record_type} record registered.")
 
         print("\nQuery executed successfully\n")
 
 
-   # except dns.resolver.NXDOMAIN:
-   #     print(f"***Error, The DNS query name {domain_to_resolve} does not exist.\n")
+    
+
+    except dns.resolver.NXDOMAIN:
+        print(f"***Error, The DNS query name {domain.lower()} does not exist.\n")
     
     except dns.resolver.Timeout:
         print("***Error: DNS query timed out.\n")
@@ -69,33 +72,19 @@ def PRT_Record():
     Ip_To_Resolve = input(str("Enter an IPv4 (x.x.x.x; from 0 to 255) address to check it's PRT record (associated domain name): ")) #the dns.resolver.resolve_address function only accepts strings as an argument.
      
     try:
-        if Ip_To_Resolve == "":
-            print("***Error, the entered IP address is blank.\n")
+        if not Ip_To_Resolve:
+            print("***Error, the entered IP address is blank.\n")        
 
-        elif isinstance(Ip_To_Resolve, str):
+        if icmp_tools.IP_address_format_validation(Ip_To_Resolve) == False:
+            return
 
-            Separated_Ip_Numbers = Ip_To_Resolve.split(".")
+            
+        PRT_Record_Resolution = dns.resolver.resolve_address(Ip_To_Resolve)
 
-            if len(Separated_Ip_Numbers) != 4:
-                print("***Error, the number of dot-separated segments that were introduced is not 4.\n")
-                return
+        print(f"PRT record for {Ip_To_Resolve} address:")
+        for records in PRT_Record_Resolution:
+            print(f"- {records}")
 
-            for Ip in Separated_Ip_Numbers:
-                if not Ip.isnumeric():
-                    print("***Error,one or more of the IP segments are not numeric.\n")
-                    return
-                if int(Ip) < 0 or int(Ip) > 255:
-                    print("***Error, the IP segments are not between 0 and 255.\n")
-                    return
-                
-            PRT_Record_Resolution = dns.resolver.resolve_address(Ip_To_Resolve)
-
-            print(f"PRT record for {Ip_To_Resolve} address:")
-            for records in PRT_Record_Resolution:
-                print(f"- {records}")
-
-        else: 
-            print("***Error, provided IP format is not valid.\n")
         
     except dns.resolver.NXDOMAIN:
         print(f"***Error, the IP {Ip_To_Resolve} does not have any PRT record registered.\n")
@@ -105,9 +94,3 @@ def PRT_Record():
     
     except dns.resolver.NoNameservers:
         print("***Error: No DNS servers available\n")
-        
-    
-     
-    
-
-     
--- tools/icmp_tools.py
+++ tools/icmp_tools.py
@@ -1,43 +1,70 @@
-from icmplib import ping
 import icmplib.exceptions 
+import subprocess
+import platform
 
-def Ping_feature():
+from icmplib import ping, traceroute
 
-    Address_To_Ping = input(str("Enter an IPv4 (x.x.x.x; from 0 to 255) address or hostname to ping: ")) #the dns.resolver.resolve_address function only accepts strings as an argument.  
+def IP_address_format_validation(ip_add):
 
-    if Address_To_Ping == "":
-        print("***Error, the entered address is blank.\n")
+    Separated_Ip_Numbers = ip_add.split(".")    
 
-    elif isinstance(Address_To_Ping, str):
+    for Ip in Separated_Ip_Numbers:
+        if not Ip.isnumeric():
+            print(f"***Error, one or more of the IP segments are not numeric ({Ip}).\n")
+            return False
+        if int(Ip) < 0 or int(Ip) > 255:
+            print(f"***Error, one or more of the the IP segments are not between 0 and 255 ({Ip}).\n")
+            return False 
 
-        try:            
+def Ping_Feature(Address_To_Validate):   
+     
+    try:
 
-            if "." in Address_To_Ping: #Case for when address is IPv4
-                Separated_Ip_Numbers = Address_To_Ping.split(".")
+        if "." in Address_To_Validate and len(Address_To_Validate.split(".")) == 4:   
+            if IP_address_format_validation(Address_To_Validate) == False:
+                 return
 
-                if len(Separated_Ip_Numbers) != 4:
-                    print("***Error, the number of dot-separated segments that were introduced is not 4.\n")
-                    return
-
-                for Ip in Separated_Ip_Numbers:
-                    if not Ip.isnumeric():
-                        print("***Error, one or more of the IP segments are not numeric.\n")
-                        return
-                    if int(Ip) < 0 or int(Ip) > 255:
-                        print("***Error, the IP segments are not between 0 and 255.\n")
-                        return
-                       
-            host = ping(Address_To_Ping)
-            print("\n")
-
-            if not host.is_alive:
-                print("Destination cannot be reached.")
-            else:
-                print(host)
+        response = ping(Address_To_Validate)
         
-        except icmplib.exceptions.NameLookupError:
-            print("The hostname or address entered cannot be resolved.")
-            
-#        except icmplib.exceptions.DestinationUnreachable:
-#            print("****Error, the hostname or address entered cannot be reached.")
+        if not response.is_alive:
+            print("Destination cannot be reached.")
+        else:
+            print(response)
+
+    except icmplib.exceptions.NameLookupError:
+        print("The hostname or address entered cannot be resolved.")
+
+    except icmplib.exceptions.DestinationUnreachable:
+        print("Destination cannot be reached.")
+
+def Traceroute_feature(Address_To_Validate):
+
+    if "." in Address_To_Validate and len(Address_To_Validate.split(".")) == 4:   
+        if IP_address_format_validation(Address_To_Validate) == False:
+            return
+    os_name = platform.system()
+    subprocess.run(["tracert", Address_To_Validate] if os_name == "Windows" else ["traceroute", Address_To_Validate])
+ 
+
+def ICMP_tools(selection): # 3. ICMP IPv4 ping; 4. ICMP IPv4 traceroute
+
+    match selection:
+        case 3:
+              option_print = "ping"
+        case 4:
+              option_print = "trace over route"
+
+    Address_To_Validate = input(f"Enter an IPv4 (x.x.x.x; from 0 to 255) address or hostname to {option_print}: ") #the dns.resolver.resolve_address function only accepts strings as an argument.  
+
+    if Address_To_Validate == "":
+        print("***Error, the entered address is blank.\n")
+        return  
+
+    match selection:
+        case 3: #Ping
+                Ping_Feature(Address_To_Validate)
+        case 4: #Traceroute
+                Traceroute_feature(Address_To_Validate)
+
+
 
